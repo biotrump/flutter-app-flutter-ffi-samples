03d1376327403a224d6c7e5678f185fc06c5e370
Thomas Tsai <thomas@quantums.ai>
2020-04-30 18:22:56 +0800

    [ios]flutter creates two frameworks for ios. The first is flutter.framework for flutter engine and the second is the App.framework for the real ios app. If the App.framework is built for the simulaotor, it won't pass the build for a real phone. Vice versa. So the ios/App.framework should be removed or renamed before different target devices.

--- packages/simple/lib/simple.dart
+++ packages/simple/lib/simple.dart
@@ -1,12 +1,14 @@
 import 'dart:async';
 import 'dart:ffi' as ffi;
 
+//dart call convention
 typedef NativeBinaryOp = ffi.Int32 Function(ffi.Int32, ffi.Int32);
+//c call convention : @_cdecl("nativeSum")
 typedef BinaryOp = int Function(int, int);
 
 class Simple {
   static Future<String> get platformVersion async {
-    final ex = ffi.DynamicLibrary.executable();
+    final ex = ffi.DynamicLibrary.process();  //executable()
     final nativeSum = ex.lookupFunction<NativeBinaryOp, BinaryOp>("nativeSum");
     final result = nativeSum(31, 63);
     return result.toString();
